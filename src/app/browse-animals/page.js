ede087087097357c6b9e418d748cc53d10a9b4bb
Notoriousmb10 <yashbhingarde9@gmail.com>
2025-01-07 17:28:24 +0530

    Added Navbar to browse-animals page

--- src/app/browse-animals/page.js
+++ src/app/browse-animals/page.js
@@ -9,25 +9,26 @@ import AdoptedPets from "@/components/uicomponents/BrowseAnimals/AdoptedPets";
 import Adoption from "@/components/uicomponents/BrowseAnimals/Adoption";
 import SuccessStories from "@/components/uicomponents/BrowseAnimals/SuccessStories";
 import AdoptionSidebar from "@/components/uicomponents/BrowseAnimals/AdoptionSidebar";
+import HomeNavbar from "@/components/uicomponents/HomePage/HomeNavbar";
 const page = () => {
   const [selectedTab, setSelectedTab] = useState("Adoptions");
 
   return (
-    <div className="mx-20 my-20 flex flex-col gap-24">
-      <SuccessStories />
-      <div className="flex flex-row gap-12 border-2 p-6 rounded-lg">
-        <div className="sticky top-0">
-
-        <AdoptionSidebar currentTab={(value) => setSelectedTab(value)} /> 
-        </div>
-          <div>
-
-        {selectedTab === "Adoptions" && <Adoption />}
-        {selectedTab === "Adopted" && <AdoptedPets />}
+    <>
+      <HomeNavbar />
+      <div className="mx-20 my-10 flex flex-col gap-24">
+        <SuccessStories />
+        <div className="flex flex-row gap-12 border-2 p-6 rounded-lg">
+          <div className="sticky top-0">
+            <AdoptionSidebar currentTab={(value) => setSelectedTab(value)} />
           </div>
-
+          <div>
+            {selectedTab === "Adoptions" && <Adoption />}
+            {selectedTab === "Adopted" && <AdoptedPets />}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
