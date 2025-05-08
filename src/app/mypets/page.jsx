624c9359320f6870d3a7cfa2b10a509f7f73d02d
Notoriousmb10 <yashbhingarde9@gmail.com>
2025-05-08 21:12:39 +0530

    Fixed usecallback

--- src/app/mypets/page.jsx
+++ src/app/mypets/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MyPets from "../../components/uicomponents/MyPetsPage/MyPets";
 import AddPets from "../../components/uicomponents/MyPetsPage/AddPets";
 import HomeNavbar from "@/components/uicomponents/HomePage/HomeNavbar";
